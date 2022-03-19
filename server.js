2aa238ed699d1adf967ec3dee4ac82ab824ed93a
hadas <hadass91@gmail.com>
2022-03-19 19:10:30 +0200

    socket fixed to broadcast when user logged in

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
     const corsOptions = {
-        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3001'],
         credentials: true
     }
     app.use(cors(corsOptions))
